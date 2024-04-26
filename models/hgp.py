c81d76dc0a509aa62be1c12263bd4feda61f2387
antimoad <riccardo.salami@outlook.it>
2024-04-26 15:41:47 +0200

    Update hgp.py

--- models/hgp.py
+++ models/hgp.py
@@ -193,6 +193,6 @@ class HGP(BaseModel):
                     gaussians.append(number)
                     gaussians.append(torch.mean(features[true_labels == client_label], 0))
                     gaussians.append(torch.std(features[true_labels == client_label], 0) ** 2)
-                    print(torch.norm(gaussians[2], p=2))
+                    print(number, torch.norm(gaussians[1], p=2), torch.norm(gaussians[2], p=2))
                     client_statistics[client_label] = gaussians
             self.clients_statistics = client_statistics
