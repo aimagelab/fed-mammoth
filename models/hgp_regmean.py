def6a0d07b0880ccd5e958bdb646005ae91f5fe0
antimoad <riccardo.salami@outlook.it>
2024-04-30 17:08:19 +0200

    Update hgp_regmean.py

--- models/hgp_regmean.py
+++ models/hgp_regmean.py
@@ -257,15 +257,14 @@ class HGPRegmean(HGP):
                 counter = 0
                 dataloader.dataset.data = train_dataset_data[train_dataset_targets == clas]
                 dataloader.dataset.targets = train_dataset_targets[train_dataset_targets == clas]
-                if len(dataloader.dataset.data) > 5:
-                    while counter <= 1000:
-                        for inputs, labels in dataloader:
-                            inputs = inputs.to(self.device)
-                            label_list.append(labels)
-                            feature_list.append(self.network(inputs, pen=True))
-                            counter += feature_list[-1].shape[0]
-                            if counter >= 1000:
-                                break
+                while counter <= 1000:
+                    for inputs, labels in dataloader:
+                        inputs = inputs.to(self.device)
+                        label_list.append(labels)
+                        feature_list.append(self.network(inputs, pen=True))
+                        counter += feature_list[-1].shape[0]
+                        if counter >= 1000:
+                            break
 
             dataloader.dataset.data = train_dataset_data
             dataloader.dataset.targets = train_dataset_targets
