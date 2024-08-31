729a3fe9fba0b51d608b424895cc2d4ffafc48f2
Riccardo Salami <riccardo.salami@outlook.it>
2024-08-31 20:19:47 +0200

    Update lora_regmean_fisher.py

--- models/lora_regmean_fisher.py
+++ models/lora_regmean_fisher.py
@@ -88,7 +88,7 @@ class LoraRegmeanFisher(LoraRegMean):
 
     def end_round_client(self, dataloader: DataLoader):
         Lora.end_round_client(self, dataloader)
-        self.set_optimization(fabric=True)
+        self.set_optimization_cur_task(fabric=True)
         RegMean.end_round_client(self, dataloader)  # retrieves Gram matrices from hooks
         self.optimization_dict = self.get_optimization_dict()
         if len(self.classes) == 0:
@@ -107,9 +107,15 @@ class LoraRegmeanFisher(LoraRegMean):
             torch.set_float32_matmul_precision("high")
             self.set_optimization(fabric=False)
             fisher = compute_fisher_expectation_fabric(
-                network=self, data_loader=dataloader, device=self.device, classes=self.classes, fabric=None, parameters=list(self.cur_B.values()), maxiter=self.fisher_maxiter
+                network=self,
+                data_loader=dataloader,
+                device=self.device,
+                classes=self.classes,
+                fabric=None,
+                parameters=list(self.cur_B.values()),
+                maxiter=self.fisher_maxiter,
             ).reshape(-1)
-            self.set_optimization(fabric=True)
+            self.set_optimization_cur_task(fabric=True)
             self.fisher = {}
             counter = 0
             for key in self.lora_keys:
@@ -157,14 +163,14 @@ class LoraRegmeanFisher(LoraRegMean):
                 for key in self.lora_keys:
                     if "weight" in key and self.middle_names.get(key) is not None and not "head" in key:
                         name = self.middle_names[key]
-                        #print(f"key: {key}, eps: {eps}")
+                        # print(f"key: {key}, eps: {eps}")
                         B = torch.stack(
                             [((fish[key].to(dtype) + eps) * B_[key].to(dtype)) for fish, B_ in zip(fishers, cl_B)]
                         ).sum(0) / (torch.stack([(fish[key].to(dtype) + eps) for fish in fishers]).sum(0))
-                        #print(f"fishers mins: {[(fishers[i][key].to(dtype) + eps).min() for i in range(len(fishers))]}")
-                        #print(
+                        # print(f"fishers mins: {[(fishers[i][key].to(dtype) + eps).min() for i in range(len(fishers))]}")
+                        # print(
                         #    f"B mins: {B.min()}, nans: {B.isnan().sum()}, infs: {B.isinf().sum()} max: {B.max()}, mean: {B.mean()}"
-                        #)
+                        # )
                         G = torch.stack([client["grams"][name].to(dtype) for client in client_info]).sum(0)
                         E = torch.stack(
                             [
@@ -177,7 +183,7 @@ class LoraRegmeanFisher(LoraRegMean):
                         self.cur_B[key] = nn.Parameter(B.detach().clone().to(torch.float32)).to(self.device)
                         self.cur_A[key] = nn.Parameter(A.detach().clone().to(torch.float32)).to(self.device)
                         del B, G, E, A
-                        # self.fed_weights[key] = self.fed_weights[key].to("cpu")
+                        self.fed_weights[key] = self.cur_B[key] @ self.cur_A[key]
                         # self.fed_weights[key] += (
                         #    torch.stack(
                         #        [
@@ -220,10 +226,50 @@ class LoraRegmeanFisher(LoraRegMean):
 
             self.set_optimization()
 
-    def set_optimization(self, fabric=True):
+    def set_optimization_cur_task(self, fabric=True):
         sd = self.network.state_dict()
         opt_dict = self.get_optimization_dict(fabric=fabric)
         if not self.lora_head:
             for key in self.head_keys:
                 opt_dict[key] = sd[key]
         self.optimization_dict = opt_dict
+
+    def set_optimization(self, fabric=True):
+        with torch.no_grad():
+            self.optimization_dict = deepcopy(dict(self.network.state_dict()))
+            for key in self.optimization_dict.keys():
+                self.optimization_dict[key] = self.optimization_dict[key].to(self.device)
+            for key in self.lora_keys:
+                self.old_delta[key].requires_grad = False
+                self.cur_B[key].requires_grad = False
+                self.cur_A[key].requires_grad = False
+                self.fed_weights[key].requires_grad = False
+                self.fed_weights[key] = self.fed_weights[key].to(self.device)
+                self.optimization_dict[key] = self.optimization_dict[key].to(self.device)
+            if "run_sum" in self.cl_merge:
+                for key in self.lora_keys:
+                    if self.cur_task > 0:
+                        self.optimization_dict[key] += self.old_delta[key]
+                    self.optimization_dict[key] += self.fed_weights[key]
+            elif "run_mean" in self.cl_merge:
+                for key in self.lora_keys:
+                    if self.cur_task > 0:
+                        tmp = (self.old_delta[key] * self.cur_task) + self.fed_weights[key]
+                        self.optimization_dict[key] += tmp / (self.cur_task + 1)
+                    else:
+                        self.optimization_dict[key] += self.fed_weights[key]
+            elif "individual" in self.cl_merge:
+                if "sum" in self.cl_merge:
+                    for key in self.lora_keys:
+                        if self.cur_task > 0:
+                            tmp = (self.old_delta[key] * self.cur_task) + self.fed_weights[key]
+                            self.optimization_dict[key] += tmp
+                        else:
+                            self.optimization_dict[key] += self.fed_weights[key]
+                elif "mean" in self.cl_merge:
+                    for key in self.lora_keys:
+                        if self.cur_task > 0:
+                            tmp = (self.old_delta[key] * self.cur_task) + self.fed_weights[key].detach()
+                            self.optimization_dict[key] += tmp / (self.cur_task + 1)
+                        else:
+                            self.optimization_dict[key] += self.fed_weights[key].detach()
