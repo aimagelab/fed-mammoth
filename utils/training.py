42113e3e4502c9f0fac89e659d29c53f60d7670a
Pietro Buzzega <pietrobuzzega@gmail.com>
2024-09-10 13:31:24 +0200

    fix typo fstring

--- utils/training.py
+++ utils/training.py
@@ -54,7 +54,7 @@ def train(
 ) -> None:
 
     if args["wandb"]:
-        name = f"{args["nickname"]}_{args['dataset']}_{args['model']}_rnds{args['num_comm_rounds']}_clnts{args['num_clients']}_epchs{args['num_epochs']}_bs{args['batch_size']}_lr{args['lr']}"
+        name = f"{args['nickname']}_{args['dataset']}_{args['model']}_rnds{args['num_comm_rounds']}_clnts{args['num_clients']}_epchs{args['num_epochs']}_bs{args['batch_size']}_lr{args['lr']}"
         wandb.init(project=args["wandb_project"], entity=args["wandb_entity"], config=args, name=name)
         # args.wandb_url = wandb.run.get_url()
 
