ff63cc6120bc68a30814aa55614e789c6bbd790e
Pietro Buzzega <pietrobuzzega@gmail.com>
2024-12-05 12:47:54 +0100

    utils for fisherAVG

--- networks/utils.py
+++ networks/utils.py
@@ -17,3 +17,18 @@ class BaseNetwork(nn.Module):
             cand_params = new_params[progress : progress + torch.tensor(pp.size()).prod()].view(pp.size())
             progress += torch.tensor(pp.size()).prod()
             pp.data = cand_params
+
+    def get_grads(self, discard_classifier=False) -> torch.Tensor:
+        grads = []
+        for kk, pp in list(self.named_parameters()):
+            if not discard_classifier or not 'head' in kk:
+                grads.append(pp.grad.view(-1))
+        return torch.cat(grads)
+    
+    def set_grads(self, new_grads: torch.Tensor, discard_classifier=False) -> torch.Tensor:
+        progress = 0
+        for pp in list(self.parameters() if not discard_classifier else self._features.parameters()):
+            cand_grads = new_grads[progress: progress +
+                                   torch.tensor(pp.size()).prod()].view(pp.size())
+            progress += torch.tensor(pp.size()).prod()
+            pp.grad = cand_grads
